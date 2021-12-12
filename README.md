b40efb9ba7599a2185a64d166151d928d0b8432b
UDomingos <49692053+UDomingos@users.noreply.github.com>
2021-12-12 20:12:32 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# api.domingos
+# api.domingos
---
+++ knexfile.js
@@ -0,0 +1,37 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database/dev.sqlite3'
+    },
+    migrations:{
+      directory:'./database/migrations',
+    },
+    useNullAsDefault: true,
+    pool:{
+      afterCreate: function(conn, done){
+        conn.run('PRAGMA foreign_keys = ON',done);
+      },
+    },
+    seeds:{
+      directory:'./database/seeds',
+    },
+  },
+production:{
+  client:"pg",
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl:{ rejectUnauthhorized: false },
+  },
+  migrations:{
+    directory:'./database/migrations',
+  },
+
+seeds:{
+  directory:'./database/seeds',
+},
+},
+};
